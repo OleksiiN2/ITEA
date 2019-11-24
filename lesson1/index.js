ba4bbe239682cd3936705374369ea9c04e9926f7
Недосєк Олексій Олегович <nedosek.o@agromat.intranet>
2019-11-24 15:36:37 +0200

    first commit: lesson 1

---
+++ lesson1/index.js
@@ -0,0 +1,98 @@
+window.onload = function(){
+    
+    //добавляем класс при клике на элемент(выбранный элемент)
+    document.getElementById("list-items").addEventListener("click",function(e) {
+        //console.log(e.target);
+        if (e.target && e.target.matches("li")) {
+
+          let elements = document.getElementsByClassName("selected");
+          for (i = 0; i < elements.length; i++) {
+              elements[i].className = "";
+          }
+          e.target.className = "selected"; 
+          
+         }
+    });
+}
+
+let elements = document.getElementsByClassName("selected");
+
+// Выбрать первый элемент 
+function selectFirst(){
+    for (i = 0; i < elements.length; i++) {
+        elements[i].className = "";
+    }
+    let ul = document.getElementById("list-items");
+    ul.firstElementChild.className = "selected";
+}
+
+//Выбрать последний элемент
+function selectLast(){
+    for (i = 0; i < elements.length; i++) {
+        elements[i].className = "";
+    }
+    let ul = document.getElementById("list-items");
+    ul.lastElementChild.className = "selected";
+}
+
+//Выбрать следующий элемент
+function selectNext(){
+    let el = document.getElementsByClassName("selected")[0];
+    let nextEl = el.nextElementSibling;
+    
+    if(nextEl){
+        nextEl.className = "selected";
+        el.className = "";
+    } else {
+        selectFirst();
+    }
+}
+
+//Выбрать предыдущий элемент
+function selectPrevious(){
+   
+    let el = document.getElementsByClassName("selected")[0];
+    let prevEl = el.previousElementSibling;
+   
+    if(prevEl){
+        prevEl.className = "selected";
+        el.className = "";
+    } else {
+        selectLast();
+    }
+}
+
+//Добавить элемент
+function addToEnd(){
+    for (i = 0; i < elements.length; i++) {
+        elements[i].className = "";
+    }
+    let ul = document.getElementById("list-items");
+    let newLi = document.createElement("li");
+    newLi.innerText = "element";
+    newLi.className = "selected";
+    ul.appendChild(newLi);
+   // ul.lastElementChild.className = "selected";
+}
+
+//Удалить эелемент
+function deleteElement(){
+
+    let ul = document.getElementById("list-items");
+    let el = document.getElementsByClassName("selected")[0];
+    el.parentNode.removeChild(el);
+    ul.firstElementChild.className = "selected";
+}
+
+//Добавить в начало
+function addToStart(){
+    for (i = 0; i < elements.length; i++) {
+        elements[i].className = "";
+    }
+    let ul = document.getElementById("list-items");
+    let newLi = document.createElement("li");
+    let el = ul.firstElementChild;
+    newLi.innerText = "element";
+    newLi.className = "selected";
+    ul.insertBefore(newLi,el);
+}
